57e618645501f81ff9df3ce21bcd3bf5578acd71
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-09-07 20:18:10 +0530

    usermodel update

--- Models/userModel.js
+++ Models/userModel.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        required: true
+        default: "student"
     },
     isAdmin: {
         type: Boolean,
