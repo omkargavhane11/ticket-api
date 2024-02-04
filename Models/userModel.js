45c3758d75745a131547b1f006db8d21bc1e0580
Omkar Gavhane <126314263+ogomkar@users.noreply.github.com>
2024-02-04 21:01:11 +0530

    add isEmailVerified to user schema

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,40 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        min: 3,
+      type: String,
+      required: true,
+      min: 3,
     },
     email: {
-        type: String,
-        required: true,
-        min: 5,
-        max: 20,
-        unique: true
+      type: String,
+      required: true,
+      min: 5,
+      max: 20,
+      unique: true,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
     },
     password: {
-        type: String,
-        required: true,
-        min: 5,
-        max: 20
+      type: String,
+      required: true,
+      min: 5,
+      max: 20,
     },
     contactNo: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     role: {
-        type: String,
-        default: "student"
+      type: String,
+      default: "student",
     },
     isAdmin: {
-        type: Boolean,
-        default: false
-    }
-
-},
-    { timestamps: true }
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema)
-
+module.exports = mongoose.model("User", userSchema);
