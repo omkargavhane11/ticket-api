01b1d62f6728943bc283a4212ba38648a75f2892
Omkar Gavhane <126314263+ogomkar@users.noreply.github.com>
2024-03-02 19:13:14 +0530

    set cors to netlify frontend website

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const authRouter = require("./Routes/authRoute");
 dotenv.config();
 const app = express();
 app.use(express.json())
-// app.use(cors(
-//     {
-//         origin:
-//             "https://myticket77.netlify.app"
-//     }
-// ))
-app.use(cors());
+app.use(cors(
+    {
+        origin:
+            "https://myticket77.netlify.app"
+    }
+))
+// app.use(cors());
 
 const PORT = process.env.PORT
 
